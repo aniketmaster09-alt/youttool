32eb81e9d7088a1ed542f7cb07d8ff4443db2b39
aniketmaster09-alt <aniketmaster09@gmail.com>
2025-11-03 23:06:02 +0530

    Update download.js

--- netlify/functions/download.js
+++ netlify/functions/download.js
@@ -1,7 +1,4 @@
 const ytdl = require('@distube/ytdl-core');
-const { exec } = require('child_process');
-const { promisify } = require('util');
-const execAsync = promisify(exec);
 
 exports.handler = async (event, context) => {
     const headers = {
@@ -33,45 +30,39 @@ exports.handler = async (event, context) => {
             };
         }
 
-        const { stdout } = await execAsync(`yt-dlp -J "${url}"`);
-        const info = JSON.parse(stdout);
+        const info = await ytdl.getInfo(url);
         const medias = [];
 
         // Process all formats
         info.formats.forEach(format => {
-            if (format.url) {
-                const isVideo = format.vcodec && format.vcodec !== 'none';
-                const isAudio = format.acodec && format.acodec !== 'none';
-                
-                const media = {
-                    formatId: parseInt(format.format_id),
-                    label: `${format.ext} (${format.height ? format.height + 'p' : format.abr ? format.abr + 'kb/s' : 'audio'})`,
-                    type: isVideo ? 'video' : 'audio',
-                    ext: format.ext,
-                    quality: `${format.ext} (${format.height ? format.height + 'p' : format.abr ? format.abr + 'kb/s' : 'audio'})`,
-                    width: format.width || null,
-                    height: format.height || null,
-                    url: format.url,
-                    bitrate: format.tbr ? Math.round(format.tbr * 1000) : format.abr ? Math.round(format.abr * 1000) : null,
-                    fps: format.fps || null,
-                    audioQuality: isAudio ? (format.abr > 100 ? 'AUDIO_QUALITY_MEDIUM' : 'AUDIO_QUALITY_LOW') : null,
-                    audioSampleRate: format.asr ? format.asr.toString() : null,
-                    mimeType: `${isVideo ? 'video' : 'audio'}/${format.ext}${format.vcodec ? `; codecs="${format.vcodec}${isAudio ? ', ' + format.acodec : ''}"` : ''}`,
-                    duration: Math.round(info.duration),
-                    is_audio: isAudio,
-                    extension: format.ext
-                };
-                medias.push(media);
-            }
+            const media = {
+                formatId: parseInt(format.itag),
+                label: `${format.container} (${format.qualityLabel || format.audioBitrate + 'kbps'})`,
+                type: format.hasVideo ? 'video' : 'audio',
+                ext: format.container,
+                quality: format.qualityLabel || format.audioBitrate + 'kbps',
+                width: format.width || null,
+                height: format.height || null,
+                url: format.url,
+                bitrate: format.bitrate || null,
+                fps: format.fps || null,
+                audioQuality: format.hasAudio ? 'AUDIO_QUALITY_MEDIUM' : null,
+                audioSampleRate: format.audioSampleRate || null,
+                mimeType: format.mimeType,
+                duration: parseInt(info.videoDetails.lengthSeconds),
+                is_audio: format.hasAudio && !format.hasVideo,
+                extension: format.container
+            };
+            medias.push(media);
         });
 
         const response = {
             url: url,
             source: 'youtube',
-            title: info.title,
-            author: info.uploader || '',
-            thumbnail: info.thumbnail,
-            duration: Math.round(info.duration),
+            title: info.videoDetails.title,
+            author: info.videoDetails.author.name,
+            thumbnail: info.videoDetails.thumbnails[0].url,
+            duration: parseInt(info.videoDetails.lengthSeconds),
             medias: medias,
             type: 'multiple',
             error: false,
