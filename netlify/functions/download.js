85d97a5a08efb26c4506dad4f1797433f7f771b0
aniketmaster09-alt <aniketmaster09@gmail.com>
2025-11-04 08:21:20 +0530

    Update download.js

--- netlify/functions/download.js
+++ netlify/functions/download.js
@@ -2,7 +2,7 @@ const ytdl = require('@distube/ytdl-core');
 
 exports.handler = async (event, context) => {
     const headers = {
-        'Access-Control-Allow-Origin': 'https://youttool.netlify.app',
+        'Access-Control-Allow-Origin': 'https://downr.org',
         'Access-Control-Allow-Headers': 'Content-Type',
         'Access-Control-Allow-Methods': 'POST, OPTIONS'
     };
